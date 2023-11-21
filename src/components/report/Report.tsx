0c4a5520c690ed5fc8fd7725edba4ec2b8e54d29
richardhan <hxy.314@gmail.com>
2023-11-21 14:10:10 +1100

    add accodion

--- src/components/report/Report.tsx
+++ src/components/report/Report.tsx
@@ -1,11 +1,31 @@
+import { Table } from 'antd';
 import { ReportProps } from '../../features/Home'
 
-export const Report = (report: ReportProps) => {
-  console.log(report)
+export const columns = [
+  {
+    title: "Bus Id",
+    dataIndex: "busId"
+  },
+  {
+    title: "Route Variant",
+    dataIndex: "routeVariant",
+    render: (value: number) => {
+      return value
+    }
+  },
+  {
+    title: 'Status',
+    dataIndex: 'deviationFromTimetable',
+    render: (value: number) => {
+      return value
+    }
+  }
+];
+
+export const Report = (reports: ReportProps) => {
   return (
     <div>
-      <h2>Bus Report</h2>
+      <Table dataSource={reports.busData} rowKey="busId" columns={columns} pagination={false} />
     </div>
   )
 }
-
--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,13 +1,15 @@
+import { Collapse } from 'antd';
 import busData from "../data/bus-services-data.json";
 import { Report } from '../components/report/Report';
+
 
 export interface ReportProps {
   organisation: string;
   date: string;
-  busData: busDataDetails[];
+  busData: BusDataDetails[];
 }
 
-interface busDataDetails {
+export interface BusDataDetails {
   busId: string;
   routeVariant: string;
   deviationFromTimetable: number | null;
@@ -16,14 +18,17 @@ interface busDataDetails {
 export const Home = () => {
   const data = busData.data;
 
+  const items = data.map((report: ReportProps, index: number) => {
+    return {
+      label: report.organisation + "-" + report.date,
+      children: <Report key={index} {...report} />
+    };
+  })
+
   return (
     <div>
       <h1>Bus Reports</h1>
-      <div>
-        {data.map((report: ReportProps) => {
-          return <Report key={report.organisation} {...report} />;
-        })}
-      </div>
+      <Collapse items={items} bordered={false} />
     </div>
   )
 }
